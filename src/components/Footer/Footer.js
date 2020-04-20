a657c2e5790e910f659c4fabddd93392065063a3
Alisher Begmatov <alisherbegmatov@yandex.com>
2020-04-19 17:45:54 -0700

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css';
+
+
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <h1>Contact us</h1>
+        <p>Swing by for a cup of coffee, or leave us a message:</p>
+        <p>sfpopos@popos.org</p>
+        <h3>SF copyright</h3>
+      </footer>
+    </div>
+  )
+}
+
+
+
+export default Footer
